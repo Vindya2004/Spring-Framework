e8591e1c92d5d9db6d87bed931a1104aee987bb0
Vindya2004 <vindyamadubhashini@gmail.com>
2025-06-26 10:01:06 +0530

    update interbean dependancy

--- 04_FullModeVsLightMode/src/main/java/org/example/config/AppConfig.java
+++ 04_FullModeVsLightMode/src/main/java/org/example/config/AppConfig.java
@@ -9,20 +9,21 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 @ComponentScan(basePackages = "org.example.bean")
 public class AppConfig {
+ //interbean dependancy satisfied
 
-    @Bean
-    public SpringBeanOne springBeanOne() {
-        SpringBeanTwo springBeanTwo1 =  springBeanTwo();
-        SpringBeanTwo springBeanTwo2 =  springBeanTwo();
-        System.out.println(springBeanTwo1);
-        System.out.println(springBeanTwo2);
-
-        return new SpringBeanOne();
-    }
-
-    @Bean
-    public SpringBeanTwo springBeanTwo() {
-        return new SpringBeanTwo();
-    }
+//    @Bean
+//    public SpringBeanOne springBeanOne() {
+//        SpringBeanTwo springBeanTwo1 =  springBeanTwo();
+//        SpringBeanTwo springBeanTwo2 =  springBeanTwo();
+//        System.out.println(springBeanTwo1);
+//        System.out.println(springBeanTwo2);
+//
+//        return new SpringBeanOne();
+//    }
+//
+//    @Bean
+//    public SpringBeanTwo springBeanTwo() {
+//        return new SpringBeanTwo();
+//    }
 
 }
