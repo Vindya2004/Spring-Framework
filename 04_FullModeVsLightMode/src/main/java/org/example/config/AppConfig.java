8213efc071fe6ff252a4e1cc690842e55497fd71
Vindya2004 <vindyamadubhashini@gmail.com>
2025-06-26 09:45:09 +0530

    interbean

---
+++ 04_FullModeVsLightMode/src/main/java/org/example/config/AppConfig.java
@@ -0,0 +1,28 @@
+package org.example.config;
+
+
+import org.example.bean.SpringBeanOne;
+import org.example.bean.SpringBeanTwo;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+@Configuration
+@ComponentScan(basePackages = "org.example.bean")
+public class AppConfig {
+
+    @Bean
+    public SpringBeanOne springBeanOne() {
+        SpringBeanTwo springBeanTwo1 =  springBeanTwo();
+        SpringBeanTwo springBeanTwo2 =  springBeanTwo();
+        System.out.println(springBeanTwo1);
+        System.out.println(springBeanTwo2);
+
+        return new SpringBeanOne();
+    }
+
+    @Bean
+    public SpringBeanTwo springBeanTwo() {
+        return new SpringBeanTwo();
+    }
+
+}
